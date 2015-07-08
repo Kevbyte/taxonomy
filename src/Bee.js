156bcaf1e46dfff11c1f5ef15f846ab9c07003a7
Hack Reactor Students <accounts@hackreactor.com>
2015-07-08 14:45:46 -0700

    finish Taxonomy

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,16 @@
 var Bee = function() {
+  //using pseudoclassical
+  Grub.apply(this);
+  this.age = 5;
+  this.color = "yellow";
+  this.job = "keep on growing";
 };
+
+Bee.prototype = Object.create(Grub.prototype);
+Bee.prototype.constructor = Bee;
+
+var bee = new Bee();
+// bee.age 
+// --> 5
+  
 
